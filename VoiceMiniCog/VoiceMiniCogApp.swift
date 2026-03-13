282ac19cc8fcef92982149c5a92d97c22d2e306e
Azam Tolla <azamtolla@gmail.com>
2026-03-12 20:25:14 -0400

    Initial Commit

---
+++ VoiceMiniCog/VoiceMiniCogApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMiniCogApp.swift
+//  VoiceMiniCog
+//
+//  Created by Azam Tolla on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMiniCogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceMiniCogTests/VoiceMiniCogTests.swift
@@ -0,0 +1,16 @@
+//
+//  VoiceMiniCogTests.swift
+//  VoiceMiniCogTests
+//
+//  Created by Azam Tolla on 3/12/26.
+//
+
+import Testing
+
+struct VoiceMiniCogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
